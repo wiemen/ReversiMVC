f085d916eec72dd4672aaed734d5c81187f8967f
DESKTOP-1KC4UD5\wieme <wiemen1996@hotmail.com>
2022-06-15 11:58:12 +0200

    cm

--- ReversiMvcApp/wwwroot/js/templates.js
+++ ReversiMvcApp/wwwroot/js/templates.js
@@ -1,5 +1,3 @@
-Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-                return (arg1 == arg2) ? options.fn(this) : options.inverse(this); });
 Handlebars.registerPartial("fiches", Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     return "<div class=\"fiche fiche__white fade-in\"></div>\r\n";
 },"3":function(container,depth0,helpers,partials,data) {
@@ -16,6 +14,8 @@ Handlebars.registerPartial("fiches", Handlebars.template({"1":function(container
     + "\r\n"
     + ((stack1 = (lookupProperty(helpers,"ifEquals")||(depth0 && lookupProperty(depth0,"ifEquals"))||alias2).call(alias1,(depth0 != null ? lookupProperty(depth0,"beurt") : depth0),2,{"name":"ifEquals","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":5,"column":0},"end":{"line":7,"column":14}}})) != null ? stack1 : "");
 },"useData":true}));
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+                return (arg1 == arg2) ? options.fn(this) : options.inverse(this); });
 this["spa_templates"] = this["spa_templates"] || {};
 this["spa_templates"]["src"] = this["spa_templates"]["src"] || {};
 this["spa_templates"]["src"]["templates"] = this["spa_templates"]["src"]["templates"] || {};
@@ -52,19 +52,6 @@ this["spa_templates"]["src"]["templates"]["chart"]["stats"] = Handlebars.templat
     + "],\r\n        label: \"Wit\",\r\n        borderColor: \"#E6E6FA\",\r\n        fill: false\r\n      }, { \r\n        data: ["
     + alias4(((helper = (helper = lookupProperty(helpers,"zwart") || (depth0 != null ? lookupProperty(depth0,"zwart") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"zwart","hash":{},"data":data,"loc":{"start":{"line":11,"column":15},"end":{"line":11,"column":24}}}) : helper)))
     + "],\r\n        label: \"Zwart\",\r\n        borderColor: \"#000000\",\r\n        fill: false\r\n      }\r\n    ]\r\n  },\r\n  options: {\r\n    title: {\r\n      display: true,\r\n      text: 'Aantal veroverd'\r\n    }\r\n  }\r\n});";
-},"useData":true});
-this["spa_templates"]["src"]["templates"]["feedbackWidget"] = this["spa_templates"]["src"]["templates"]["feedbackWidget"] || {};
-this["spa_templates"]["src"]["templates"]["feedbackWidget"]["body"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
-        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
-          return parent[propertyName];
-        }
-        return undefined
-    };
-
-  return "<section class=\"body\">\r\n "
-    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"bericht") || (depth0 != null ? lookupProperty(depth0,"bericht") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"bericht","hash":{},"data":data,"loc":{"start":{"line":2,"column":1},"end":{"line":2,"column":12}}}) : helper)))
-    + "\r\n </section>";
 },"useData":true});
 this["spa_templates"]["src"]["templates"]["spelBord"] = this["spa_templates"]["src"]["templates"]["spelBord"] || {};
 this["spa_templates"]["src"]["templates"]["spelBord"]["bord"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data,blockParams,depths) {
@@ -111,3 +98,16 @@ this["spa_templates"]["src"]["templates"]["spelBord"]["bord"] = Handlebars.templ
     + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"bord") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":0},"end":{"line":8,"column":9}}})) != null ? stack1 : "")
     + "</table>";
 },"usePartial":true,"useData":true,"useDepths":true});
+this["spa_templates"]["src"]["templates"]["feedbackWidget"] = this["spa_templates"]["src"]["templates"]["feedbackWidget"] || {};
+this["spa_templates"]["src"]["templates"]["feedbackWidget"]["body"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<section class=\"body\">\r\n "
+    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"bericht") || (depth0 != null ? lookupProperty(depth0,"bericht") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"bericht","hash":{},"data":data,"loc":{"start":{"line":2,"column":1},"end":{"line":2,"column":12}}}) : helper)))
+    + "\r\n </section>";
+},"useData":true});
